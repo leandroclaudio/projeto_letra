ecafafb8bc21f80274969ae97265d440f85006fc
leandroclaudio <leandroclaudio@gmail.com>
2022-10-31 18:13:34 -0300

    firts commit

---
+++ letra.js
@@ -0,0 +1,21 @@
+const gabarito = "VENTO";
+
+const linha1 = document.getElementById("L1").value;
+const linha2 = document.getElementById("L2").value;
+const linha3 = document.getElementById("L3").value;
+const linha4 = document.getElementById("L4").value;
+const linha5 = document.getElementById("L5").value;
+const linha6 = document.getElementById("L6").value;
+
+
+document.addEventListener("keyup")
+
+
+
+
+
+
+
+
+
+
